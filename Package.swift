6ca5daf01f55519ec190f3538bd2ee52e3652564
Davide Mendolia <davide@gokarumi.com>
2019-06-14 10:32:12 +0200

    Add SwiftUI Apple Platforms as requirements

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "ControlFlowUI",
+    platforms: [
+        .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
